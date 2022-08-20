2f4ab80b54657636f28778dc848695b41366df2f
Aleksey <am@belimo.ru>
2022-08-20 10:23:34 +0300

    BugFix final

--- Program.cs
+++ Program.cs
@@ -26,31 +26,21 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число 3: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
 
-if(number1 > number2)
-    if(number1 > number3)
+if (number1 > number2)
+if (number1 > number3)
     {
-        Console.WriteLine("max = " + number1);
+     Console.Write(number1);
     }
-else
-{
-    if(number2 > number1)
+if (number2 > number1)
+if (number2 > number3) 
     {
-        if(number2 > number3)
-        {
-           Console.WriteLine("max = " + number2);
-        }
+    Console.Write(number2); 
     }
-}
-else
-{
-    if(number3 > number1)
+if (number3 > number1)
+if (number3 > number2) 
     {
-        if(number3 > number2)
-        {
-            Console.WriteLine("max = " + number3);
-        }
-    }   
-}
+    Console.Write(number3); 
+    }
 */
 
 /*
